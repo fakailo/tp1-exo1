290999ffc6ddb3ef8708569b1ec69206c7ea1c22
fakailo <68045774+fakailo@users.noreply.github.com>
2020-07-16 17:44:59 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(x, 0)
+    direction = -1
+    led.plot(x, 0)
+    if (x <= 4) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(x, 0)
+    x += direction
+    led.plot(x, 0)
+    if (x <= 4) {
+        direction = -1
+    }
+})
+let x = 0
+let direction = 0
+direction = 1
+x = 2
+led.plot(x, 0)
+basic.forever(function () {
+    led.unplot(x, 0)
+    x += direction
+    led.plot(x, 0)
+    if (x >= 4) {
+        direction = -1
+    } else if (x <= 0) {
+        direction = 1
+    }
+    basic.pause(1000)
+})
